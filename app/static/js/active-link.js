4d23a12c6aa0924ba429011d227d37033da35125
StasyanD <stasyand330@gmail.com>
2023-03-10 16:18:59 +0200

    update active-link.js

--- app/static/js/active-link.js
+++ app/static/js/active-link.js
@@ -1,12 +1,8 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
-	let curpath = location.pathname.match(/^\/[^/]+/);
+	let curpath = location.pathname.match(/\/[^/]*/);
 	let active_link = document.querySelector('nav a[href="' + curpath + '"]');
 
 	if (active_link) active_link.className += " text-decoration-underline";
-	else {
-		if (curpath === null)
-			document.querySelector("nav a").className += " text-decoration-underline";
-	}
 });
